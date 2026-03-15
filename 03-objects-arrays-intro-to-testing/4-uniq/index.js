3d8feee62ebc0b17c35a0ee297816066e2dc32f5
existmodel <sarizava@yandex.ru>
2026-03-15 19:34:55 +0100

    add solution for 03-objects-arrays-intro-to-testing/4-uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,17 +4,6 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-  const uniqueSet = new Set(arr); //создает новый set обьект
+  const uniqueSet = new Set(arr);
   return Array.from(uniqueSet);
 }
-
-// set
-
-// возвращает новый массив уникальных значений
-// set.forEach((value, valueAgain, set) => {
-//   alert(value);
-// });
-
-// перебрать все элементы и добавить в новый массив?
-
-// Статический Array.from()метод создает новый, поверхностно скопированный Array экземпляр из итерируемого или массивоподобного объекта.
