985f4a426ba2df46a2837800231cccaf80406623
existmodel <sarizava@yandex.ru>
2026-03-20 22:28:47 +0100

    add solution for 04-opp-basic-intro-to-ts/1-column-chart

--- 04-oop-basic-intro-to-ts/1-column-chart/index.ts
+++ 04-oop-basic-intro-to-ts/1-column-chart/index.ts
@@ -1,11 +1,106 @@
-import { createElement } from "../../../shared/utils/create-element";
+import { createElement } from "../../shared/utils/create-element";
 
 interface Options {
+  data?: number[];
+  label?: string;
+  value?: number;
+  link?: string;
 
+  formatHeading?: (value: number) => string;
 }
 
 export default class ColumnChart {
-  constructor({ }: Options = {}) {
-    
+  public chartHeight = 50;
+  private data: number[];
+  private label: string;
+  private value: number;
+  private link: string;
+
+  private formatHeading?: (value: number) => string;
+
+  public element: HTMLElement;
+
+  constructor({
+    data = [],
+    label = "",
+    value = 0,
+    link = "",
+    formatHeading,
+  }: Options = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.formatHeading = formatHeading;
+
+    this.element = this.render();
+    if (!data.length) {
+      this.element.classList.add("column-chart_loading");
+    }
+  }
+
+  public update(data: number[]) {
+    this.data = data;
+    const body = this.element.querySelector('[data-element="body"]');
+
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    const columns = data
+      .map((item) => {
+        const value = Math.floor(item * scale);
+        const percent = ((item / maxValue) * 100).toFixed(0) + "%";
+        return `<div style="--value: ${value}" data-tooltip="${percent}"></div>`;
+      })
+      .join("");
+
+    if (body) {
+      body.innerHTML = columns;
+    }
+  }
+
+  private render(): HTMLElement {
+    const maxValue = Math.max(...this.data);
+
+    const scale = this.chartHeight / maxValue;
+
+    const columns = this.data
+      .map((item) => {
+        const value = Math.floor(item * scale);
+        const percent = ((item / maxValue) * 100).toFixed(0) + "%";
+        return `<div style="--value: ${value}" data-tooltip="${percent}"></div>`;
+      })
+      .join("");
+
+    //!функция для форматирования значения value (например, добавление знака валюты $).
+    const heading = this.formatHeading
+      ? this.formatHeading(this.value)
+      : String(this.value);
+
+    //!ссылка «View all» (если передана).
+    const link = this.link
+      ? `<a href="${this.link}" class="column-chart__link">View all</a>`
+      : "";
+
+    return createElement(`
+    <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+      <div class="column-chart__title">
+        ${this.label}
+        ${link}
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${heading}</div>
+        <div data-element="body" class="column-chart__chart">${columns}</div>
+      </div>
+    </div>
+  `);
+  }
+
+  public remove() {
+    this.element.remove();
+  }
+
+  public destroy() {
+    this.remove();
   }
 }
