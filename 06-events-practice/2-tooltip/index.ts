62af7b638188dcdb8cf26c31d844fee039ebb4d7
existmodel <sarizava@yandex.ru>
2026-03-29 20:37:58 +0200

    add solution for 06-events-practice/2-tooltip

--- 06-events-practice/2-tooltip/index.ts
+++ 06-events-practice/2-tooltip/index.ts
@@ -1,3 +1,70 @@
 export default class Tooltip {
+  public element: HTMLElement | null = null;
+  //Singleton
+  public static activeMessage: Tooltip | null = null;
 
+  constructor() {
+    this.element;
+
+    if (Tooltip.activeMessage) {
+      return Tooltip.activeMessage;
+    }
+    Tooltip.activeMessage = this;
+
+    this.initialize();
+  }
+
+  public render(html: string): void {
+    const element = document.createElement("template");
+    element.innerHTML = `
+      <div class="tooltip">
+        ${html}
+      </div>
+    `.trim();
+
+    this.element = element.content.firstElementChild as HTMLElement;
+    document.body.append(this.element);
+  }
+
+  private initialize() {
+    document.addEventListener("pointerover", this.showTooltip);
+    document.addEventListener("pointerout", this.hideTooltip);
+  }
+
+  private showTooltip = (event: PointerEvent) => {
+    console.log("showTooltip");
+    const tooltipElement = (event.target as HTMLElement).closest<HTMLElement>(
+      "[data-tooltip]",
+    );
+
+    if (!tooltipElement) {
+      return;
+    }
+
+    const tooltipText = tooltipElement.dataset.tooltip;
+
+    if (tooltipText !== undefined) {
+      this.render(tooltipText);
+    }
+  };
+
+  private hideTooltip = () => {
+    this.remove();
+    this.element = null;
+  };
+
+  public destroy() {
+    if (!this.element) {
+      return;
+    }
+    document.removeEventListener("pointerover", this.showTooltip);
+    document.removeEventListener("pointerout", this.hideTooltip);
+    this.remove();
+  }
+
+  private remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
